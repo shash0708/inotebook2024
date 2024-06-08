d3144658136f877cf0c523e128c3f2ce6d0c45e3
Shashank <97978650+shash0708@users.noreply.github.com>
2024-06-08 12:40:26 +0530

    hmmm

--- .history/frontend/src/index_20240608123941.js
+++ .history/frontend/src/index_20240608123941.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import * as ser
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
--- .history/frontend/src/index_20240608123952.js
+++ .history/frontend/src/index_20240608123952.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import * as serviceWorkerRegistration from "./se"
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
